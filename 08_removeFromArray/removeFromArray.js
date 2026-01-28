2177d619bb1eb644cdb0d4ec000619edd2fda535
Dlewis48 <195347648+dlewis48@users.noreply.github.com>
2026-01-27 23:01:30 -0500

    solved removeFromArray challenge

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value) {
+    var i = 0;
+    while(i<arr.length) {
+        if (arr[i] === value) {
+            arr.splice(i, 1); {
+               
+            }
+        } else{
+            ++i;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
