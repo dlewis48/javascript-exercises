c07febdfeb0c0de76a87c66ec76ae6162a391ec8
Dlewis48 <195347648+dlewis48@users.noreply.github.com>
2026-01-27 23:20:56 -0500

    sumAll solved

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    const start = Math.min(a,b);
+    const end = Math.max(a, b);
+    let total = 0;
+    for( let i = start; i<=end ; i++){
+        total += i; 
+    }
+return total;
 };
 
 // Do not edit below this line
